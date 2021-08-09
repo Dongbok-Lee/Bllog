b0e9c8f57609c21a635beea28203fcc17d5c4f4e
동복 <typoon0820@gmail.com>
2021-08-09 11:37:38 +0900

    프로젝트 생성

---
+++ src/main/java/com/bok/bllog/BllogApplication.java
@@ -0,0 +1,13 @@
+package com.bok.bllog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BllogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BllogApplication.class, args);
+	}
+
+}
