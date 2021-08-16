d7ebec986fb909e3eb6b2c97e74a5a372444a15d
동복 <typoon0820@gmail.com>
2021-08-16 20:54:39 +0900

    build

--- target/classes/static/js/main.js
+++ target/classes/static/js/main.js
@@ -1,7 +1,7 @@
 
 
 var current =0
-var slide = $('.item').find('div')
+var slide = $('.item').find('.slide')
 var count = slide.length
 
 //애니매이션 추가 함수 
@@ -11,6 +11,8 @@ function animate(index) {
     updateNav();
     
 }
+
+
 //화살표 좌우 이동 맨 끝일시 지우기 
 function updateNav(){
     var prev= $('.arrow').find('.fa-arrow-left')
@@ -54,3 +56,76 @@ $('.btn').find('a').click(function (e) {
 
 updateNav();
 
+setInterval(function(){
+    var nexindex=(current+1)%count;
+    animate(nexindex);
+
+},5000)
+
+$(window).scroll(function () {
+    var scroll_top = $(this).scrollTop();
+    console.log(scroll_top)
+    if(scroll_top>=490){
+        $('.header').css('background-color','black').css("transition","1s");
+        $('.icon').css('color','white');
+        $('.log-in').css('color','white');
+        $('.sign').css('color','white');
+
+        $('.icon').hover(function () {
+            $(this).css('color','gray');
+            
+        }, function () {
+            $(this).css('color','white')
+        }
+    );
+
+        $('.log-in').hover(function () {
+            $(this).css('color','gray');
+            
+        }, function () {
+            $(this).css('color','white')
+        }
+        );
+
+        $('.sign').hover(function () {
+            $(this).css('color','gray');
+            
+        }, function () {
+            $(this).css('color','white')
+        }
+        );
+        
+    }
+    else{
+        $('.header').css('background-color','#f0f0f0').css("transition","1s");
+        $('.icon').css('color','black');
+        $('.log-in').css('color','black');
+        $('.sign').css('color','black');
+
+        $('.icon').hover(function () {
+                $(this).css('color','white');
+                
+            }, function () {
+                $(this).css('color','black')
+            }
+        );
+
+        $('.log-in').hover(function () {
+            $(this).css('color','white');
+            
+        }, function () {
+            $(this).css('color','black')
+        }
+        );
+
+        $('.sign').hover(function () {
+            $(this).css('color','white');
+            
+        }, function () {
+            $(this).css('color','black')
+        }
+        );
+
+    }
+    
+});
